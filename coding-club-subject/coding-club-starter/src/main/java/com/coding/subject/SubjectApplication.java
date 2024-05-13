5279afe71ced3f2840a81483d65c60720bd3298b
Chen Liu <shlc020326@gmail.com>
2024-05-13 23:18:44 +0800

    修改框架

---
+++ coding-club-subject/coding-club-starter/src/main/java/com/coding/subject/SubjectApplication.java
@@ -0,0 +1,13 @@
+package com.coding.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.coding")
+public class SubjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class);
+    }
+}
